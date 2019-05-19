70539253dafcaaeba5b55f3ae2a2f0877a72233a
マツモ <machumo@matsumotonaomi-no-MacBook-Pro.local>
2019-05-19 22:54:03 +0900

    test

---
+++ main.js
@@ -0,0 +1,20 @@
+
+'use strict'
+
+// console.log('hello,world');
+
+// console.log('hello,\nworl\td');
+
+
+//定数　const
+// console.log(150 * 140);
+// console.log(150 * 160);
+
+const price = 150;
+
+console.log(price * 140);
+console.log(price * 160);
+
+//変数　let
+
+let price = 500;
